0a041f5857c089f04f062b2e7d6cf95ead96a1ea
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-29 22:24:54 -0500

    Added global contract variables + yes and no contract logic update

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import time
 from colorama import Fore
 import math 
 
-#global var
+#contract vars
+yes = 0
+no = 0
+
+#question/setup var
 yes_or_no = input("Yes or No, Contract?: ") 
 capital = input("Capital (initial invesment): ")
+
+
+#logic (where the magic happens)
+
+#yes or no contract logic
+if yes_or_no == "no" or  yes_or_no == "N":
+   yes_or_no == False
+elif yes_or_no == "yes" or yes_or_no == "Y":
+   yes_or_no == True
+else:
+   print("invalid operator")
+
+if yes_or_no == True:
+   yes == input('price per "yes" contract: ')
+else:
+   no == input('price per "no" contract: ')
+
