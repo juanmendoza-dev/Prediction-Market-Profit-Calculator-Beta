88fe6b2b3b9d706a91f0a6d5beafd7b275bbdb55
Juan Mendoza <juanmendoza6159@gmail.com>
2026-01-30 23:32:48 -0500

    updated overall structure + fixed functions issues
    Added global var, to fix functions not being able to access other var's

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import time
 from colorama import Fore
 import math 
 
-#contract vars
-yes_contract = 0
-no_contract = 0
 
 #launcher/ initial questions
 yes_or_no = input('Are you buying "Yes" or "No" Contract? ') 
@@ -13,7 +10,7 @@ capital = input("Capital (initial investment): ")
 #yes or no contract logic
 def contract():
     while True:
-        if yes_or_no == "yes" or  yes_or_no == "Y" or yes_or_no == "YES":
+        if yes_or_no == "yes" or yes_or_no == "Y" or yes_or_no == "YES":
             yes_or_no = True
         elif yes_or_no == "no" or yes_or_no == "N" or yes_or_no == "NO":
             yes_or_no = False
@@ -22,13 +19,20 @@ def contract():
 
 #contract price logic 
 def contract_Price():
+
+    #global vars
+    global yes_contract
+    global no_contract
+
     while True:
         if yes_or_no == True:
-            yes_contract == input('price per "yes" contract: ')
+            yes_contract = input('price per "yes" contract: ')
             break
         elif yes_or_no == False:
-            no_contract == input('price per "no" contract: ')
+            no_contract = input('price per "no" contract: ')
             break
         else:
             print('invalid "yes" or "no" contract price unit! Try again!')
 
+contract()
+contract_Price()
