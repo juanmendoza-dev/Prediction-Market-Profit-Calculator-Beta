520b12b2465ace6d1e1545c62fbed7a0c9ab227d
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-01 01:29:02 -0500

    Added Profit logic

--- profit.py
+++ profit.py
@@ -3,6 +3,11 @@ from colorama import Fore
 #comments are next to the inputs because colorama makes it a bit messy, sorry about that!
 # "yes" contracts = 1, and "no" contracts = 2
 
+#step one: Is it a yes or no contract?
+#step two: price per contract.
+#step three: profit logic 
+
+
 capital = input(f"{Fore.LIGHTMAGENTA_EX} Initial {Fore.LIGHTMAGENTA_EX}Capital? ") #initial capital 
 yes_no_contract = input(f'{Fore.LIGHTMAGENTA_EX}Purchased {Fore.LIGHTGREEN_EX}"Yes"{Fore.LIGHTMAGENTA_EX} or {Fore.LIGHTRED_EX}"No" {Fore.LIGHTMAGENTA_EX}Contract?: ') #Purchased "Yes" or "No" Contract
 
@@ -17,10 +22,28 @@ else:
 #contract logic (step 2)
 if contract == 1:
     yes_contract = input(f'{Fore.LIGHTMAGENTA_EX}Price Per {Fore.GREEN}"Yes" {Fore.LIGHTMAGENTA_EX}Share: ') #price per "Yes" Share
+    contract = f'{Fore.GREEN}"Yes"'
 elif contract == 2:
-    no_contract = input(f'{Fore.LIGHTMAGENTA_EX}Price Per {Fore.LIGHTRED_EX}"No" {Fore.LIGHTMAGENTA_EX}Share: ')
+    no_contract = input(f'{Fore.LIGHTMAGENTA_EX}Price Per {Fore.LIGHTRED_EX}"No" {Fore.LIGHTMAGENTA_EX}Share: ') #price per "No" share 
+    contract = f'{Fore.LIGHTRED_EX}"No"'
 else:
     print(Fore.LIGHTRED_EX +"Error please start over!!")
 
 #profit logic 
+if contract  == 1:
+    profit = (yes_contract * 0.20)
+    sell_price = (yes_contract + profit)
+elif contract == 2:
+    profit = (no_contract * 0.20)
+    sell_price = (no_contract + profit)
+else:
+    print("something went really wrong please try again!")
+    
+    
+
+
+
+#final output
+yes_profit = (f"To make 20% ({}) of {contract} contract {Fore.LIGHTMAGENTA_EX}sell at: ")
+no_profit = (f"")
 
