aadc21097ff292289e5c86ed4efd4ae81394c555
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-01 00:49:04 -0500

    Updated Spacing + Colors

--- profit.py
+++ profit.py
@@ -5,8 +5,8 @@ import math
 #comments are next to the inputs because colorama makes it a bit messy, sorry about that!
 # "yes" contracts = 1, and "no" contracts = 2
 
-capital = input(f"{Fore.LIGHTMAGENTA_EX} Initial {Fore.LIGHTGREEN_EX}Capital?") #initial capital 
-yes_no_contract = input(f'{Fore.LIGHTMAGENTA_EX}Purchased {Fore.LIGHTGREEN_EX}"Yes" {Fore.LIGHTMAGENTA_EX} or {Fore.LIGHTRED_EX}"No" {Fore.LIGHTGREEN_EX}Contract?') #Purchased "Yes" or "No" Contract
+capital = input(f"{Fore.LIGHTMAGENTA_EX} Initial {Fore.LIGHTMAGENTA_EX}Capital? ") #initial capital 
+yes_no_contract = input(f'{Fore.LIGHTMAGENTA_EX}Purchased {Fore.LIGHTGREEN_EX}"Yes"{Fore.LIGHTMAGENTA_EX} or {Fore.LIGHTRED_EX}"No" {Fore.LIGHTMAGENTA_EX}Contract?: ') #Purchased "Yes" or "No" Contract
 
 #yes or no logic (step 1)
 if yes_no_contract == "Yes" or yes_no_contract == "yes" or yes_no_contract == "Y" or yes_no_contract == "y" or yes_no_contract == "YES":
@@ -15,4 +15,4 @@ elif yes_no_contract == "No" or yes_no_contract == "no" or yes_no_contract == "N
     contract = 2
 else:
     print(Fore.LIGHTRED_EX + "invalid input please try again!")
-    
+
