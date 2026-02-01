89ff29ca2148c17339bf38def839d214089b13f1
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-01 00:35:00 -0500

    the great restart
    scraped everything restarted since I wanted to make it more clean

--- main.py
+++ main.py
@@ -2,37 +2,5 @@ import time
 from colorama import Fore
 import math 
 
-
-#launcher/ initial questions
-yes_or_no = input('Are you buying "Yes" or "No" Contract? ') 
-capital = input("Capital (initial investment): ")
-
-#yes or no contract logic
-def contract():
-    while True:
-        if yes_or_no == "yes" or yes_or_no == "Y" or yes_or_no == "YES":
-            yes_or_no = True
-        elif yes_or_no == "no" or yes_or_no == "N" or yes_or_no == "NO":
-            yes_or_no = False
-        else:
-            print("invalid operator")
-
-#contract price logic 
-def contract_Price():
-
-    #global vars
-    global yes_contract
-    global no_contract
-
-    while True:
-        if yes_or_no == True:
-            yes_contract = input('price per "yes" contract: ')
-            break
-        elif yes_or_no == False:
-            no_contract = input('price per "no" contract: ')
-            break
-        else:
-            print('invalid "yes" or "no" contract price unit! Try again!')
-
-contract()
-contract_Price()
+capital = input(f"{Fore.LIGHTMAGENTA_EX} Initial {Fore.LIGHTGREEN_EX}Capital?")
+yes_no_contract = input('Purchased "Yes" or "No" Contract?')
