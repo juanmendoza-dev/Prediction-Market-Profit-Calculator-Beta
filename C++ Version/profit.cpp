251a86168f6824a2dea352411e273c753f707e24
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-02 19:32:44 -0500

    Original Capital Added

---
+++ C++ Version/profit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    bool capital;
+    bool yes_no_contract;
+
+    //launcher
+    std::cout << "Inital Capital?: " << '\n'; //inital capital 
+    std::cin >> capital;
+    
+
+
+
+
+    return 0;
+}
